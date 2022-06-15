5b4afa7cb25a5a52e6ae9cd141c845857a1e9c1a
KJH <daeding7@naver.com>
2022-06-15 12:52:07 +0900

    가격 표현 방법 업데이트 완료 -> 층수 표현 방법 업데이트 요망

--- app/src/main/java/com/kim/roompractice1/adapters/RoomAdapter.kt
+++ app/src/main/java/com/kim/roompractice1/adapters/RoomAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import com.kim.roompractice1.R
 import com.kim.roompractice1.datas.Room
 
@@ -26,6 +27,17 @@ class RoomAdapter(
         }
 
         val row = tempRow!!
+
+        val roomData = mList[position]
+
+        val priceTxt = row.findViewById<TextView>(R.id.priceTxt)
+        val addressAndFloorTxt = row.findViewById<TextView>(R.id.addressAndFloorTxt)
+        val descriptionTxt = row.findViewById<TextView>(R.id.descriptionTxt)
+
+
+        priceTxt.text = roomData.getFormmatedPrice()
+        addressAndFloorTxt.text = "${roomData.address}, ${roomData.floor}"
+        descriptionTxt.text = roomData.description
 
         return row
     }
--- app/src/main/java/com/kim/roompractice1/datas/Room.kt
+++ app/src/main/java/com/kim/roompractice1/datas/Room.kt
@@ -1,8 +1,35 @@
 package com.kim.roompractice1.datas
 
+import android.icu.text.NumberFormat
+import android.os.Build
+import androidx.annotation.RequiresApi
+import java.util.*
+
 class Room(
     val price : Int,
     val address : String,
     val floor : Int,
     val description : String) {
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    fun getFormmatedPrice() : String {
+
+        if(this.price >= 10000){
+
+            val uk = this.price / 10000
+            val rest = this.price % 10000
+
+            return "${uk}억 ${NumberFormat.getNumberInstance(Locale.KOREA).format(rest)}"
+        }
+
+        else{
+
+            return "${NumberFormat.getNumberInstance(Locale.KOREA).format(this.price)}"
+
+
+        }
+    }
+
+
+
 }
